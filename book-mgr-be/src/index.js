63ece1b5b048f4fca5d62a5015ba83d2da4d3f89
sumeiyu <sumeiyu@gemvary.com>
2024-06-10 16:02:03 +0800

    完成注册逻辑 git commit -m 完成注册逻辑 git commit -m 完成注册逻辑

--- book-mgr-be/src/index.js
+++ book-mgr-be/src/index.js
@@ -1,13 +1,23 @@
 const koa = require('koa')
+const { connect } = require('./db')
+const body = require('koa-body')
+const registerRoutes = require('./routers')
+const cors = require('@koa/cors')
 
 const app = new koa()
 
-// 通过 app.use 注册中间件
-// 中间件本质上是一个函数，每次请求都会被执行
-// context 上下文 - 当前请求的所有信息都在里面
-app.use((context) => {})
+connect().then(() => {
+  app.use(cors())
+  app.use(body.koaBody())
 
-app.listen(3000, () => {
-  // 监听端口
-  console.log('启动成功')
+  registerRoutes(app)
+
+  // 通过 app.use 注册中间件
+  // 中间件本质上是一个函数，每次请求都会被执行
+  // context 上下文 - 当前请求的所有信息都在里面
+  
+  app.listen(3000, () => {
+    // 监听端口
+    console.log('启动成功')
+  })
 })
---
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="auth">
+    <div class="bg"></div>
+    <div class="title-info">
+      <img src="" alt="" class="image">
+      <h2 class="title">图书管理系统后台</h2>
+    </div>
+
+    <div class="form">
+      <a-tabs>
+        <a-tab-pane key="1" tab="登录">
+          <div class="item">
+            <a-input
+              v-model:value="loginForm.account"
+              size="large"
+              placeholder="账号"
+            >
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input
+              v-model:value="loginForm.password"
+              size="large"
+              placeholder="密码"
+            >
+              <template v-slot:prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a href="">忘记密码</a>
+          </div>
+          <div class="item">
+            <a-button size="large" type="primary">登录</a-button>
+          </div>
+        </a-tab-pane>
+
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input
+              v-model:value="regForm.account"
+              size="large"
+              placeholder="账号"
+            >
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input
+              v-model:value="regForm.password"
+              size="large"
+              placeholder="密码"
+            >
+              <template v-slot:prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input size="large" placeholder="邀请码">
+              <template v-slot:prefix>
+                <MailOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-button size="large" type="primary" @click="register">注册</a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons-vue';
+  import { ref } from 'vue'
+  import { auth } from '@/service'
+
+  // 数据源
+  const loginForm = ref({
+    username: '',
+    password: ''
+  })
+
+  const regForm = ref({
+    account: '',
+    password: ''
+  })
+
+  const register = () => {
+    auth.register(regForm.value.account, regForm.value.password)
+    console.log('auth',auth, regForm.value)
+  }
+</script>
+
+<style lang="scss" scpoed>
+  @import './index.scss'
+</style>
