171860e2bbabb2556a56c0a6341c2bb16c3fc1ff
sumeiyudsy <2471399792@qq.com>
2024-06-13 13:50:22 +0800

    图书列表

---
+++ book-mgr-fe/src/components/SpaceBetween/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="space-between">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .space-between {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
--- book-mgr-fe/src/views/Books/index.vue
+++ book-mgr-fe/src/views/Books/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>books</div>
+  <a-card>
+    <h2>图书列表</h2>
+
+    <a-divider />
+
+    <space-between>
+      <div class="search">
+        <a-input-search placeholder="根据书名搜索" enter-button />
+      </div>
+
+      <a-button>添加一条</a-button>
+      
+    </space-between>
+
+    <a-divider />
+    
+  </a-card>
 </template>
+
+<script setup>
+import SpaceBetween from '@/components/SpaceBetween'
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
