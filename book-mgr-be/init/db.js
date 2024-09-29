e1078d989d3b4a4d01f7f7a0a5fca051281d5db2
sumeiyudsy <2471399792@qq.com>
2024-09-29 20:37:27 +0800

    1、初始化用户admin

--- book-mgr-be/init/db.js
+++ book-mgr-be/init/db.js
@@ -5,12 +5,23 @@ const character = require('../src/helpers/character')
 const { defaultCharacters } = character
 
 const Character = mongoose.model('Character')
+const User = mongoose.model('User')
 
 connect()
   .then(async () => {
     console.log('开始初始化角色集合', )
 
-    await Character.insertMany(defaultCharacters)
+    const characherList = await Character.insertMany(defaultCharacters)
+
+    const user = new User({
+      account: 'admin',
+      password: 'admin',
+      character: characherList.find(item => (item.name === 'member'))._id
+    })
+
+    console.log('正在初始化', )
+
+    await user.save()
 
     console.log('角色集合初始化成功', )
   })
